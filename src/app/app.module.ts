f574bad1adbf1048ab920ed625e38d4182e93509
liuba <atamanyukl10@gmail.com>
2021-02-01 00:04:19 +0200

    routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ChosenCarComponent} from './components/chosen-car/chosen-car.component';
+import {CarsComponent} from './components/cars/cars.component';
+import {CarComponent} from './components/car/car.component';
+import {RouterModule} from '@angular/router';
+import {HomeComponent} from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChosenCarComponent,
+    CarsComponent,
+    CarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{
+      path: 'link/cars', component: CarsComponent, children: [{
+        path: ':id', component: ChosenCarComponent
+      }]
+    },
+      {
+        path: 'link/home', component: HomeComponent
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
